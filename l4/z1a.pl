92b45690f027f70ba0655a4d2324d6a7eb6d18c4
Voter101 <voter101@gmail.com>
2013-03-20 14:20:17 +0100

    Fix

--- l4/z1a.pl
+++ l4/z1a.pl
@@ -1,7 +1,8 @@
 % Nie działa dla dwóch nieukonkretnionych zmiennych
 len(X,L) :-
     len(X,L,0).
-len([],X,X) :- !.
+len([],X,X).
 len([_|T],L,A) :-
+    L\==A,
     A1 is A + 1,
     len(T,L,A1). 
