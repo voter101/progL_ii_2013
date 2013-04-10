1504781f5036de0d274707441602b1386cfa350a
Voter101 <voter101@gmail.com>
2013-04-10 12:10:23 +0200

    Kolejna porcja zadań. Om nom nom

--- l6/z3.pl
+++ l6/z3.pl
@@ -20,7 +20,17 @@ findMax(node(_,M,leaf),M) :- !.
 findMax(node(_,_,R), M) :-
     findMax(R,M).
 
-delete(E,node(leaf,E,leaf),leaf).
+delete(E,node(leaf,E,leaf),leaf) :- !.
+delete(E,node(L,X,R),node(L1,X,R)) :-
+    E < X, !,
+    delete(E,L,L1).
+delete(E,node(L,X,R),node(L,X,R1)) :-
+    delete(E,R,R1).
+delete(E,node(L,E,R),R1) :-
+    insertRight(R,L,R1).
+insertRight(T,node(L,E,leaf),node(L,E,T)) :- !.
+insertRight(T,node(L,E,R), node(L,E,R1)) :-
+    insertRight(T,R,R1).
 
 delMax(Source,Result,E) :-
     findMax(Source,E),
