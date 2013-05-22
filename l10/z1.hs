b70a5a4f1695607e9ec4fb080013387b044500e8
Voter101 <voter101@gmail.com>
2013-05-22 14:00:59 +0200

    Druga wersja pierwszego

--- l10/z1.hs
+++ l10/z1.hs
@@ -2,3 +2,8 @@ nat2 :: [(Integer, Integer)]
 nat2 = (0, 0) : [(case (x, y) of 
     (x, 0) -> (0, x+1)
     otherwise -> (x+1,y-1)) | (x, y) <- nat2]
+
+-- Wersja z wykładu, ładna bo w jednej linijce. :D
+
+natTWi :: [(Integer, Integer)]
+natTWi = [(y, x-y) | x <- [0..], y <- [0..x]]
